876c4bffb398907f05ff835ee8e4b654fa2e5f59
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-31 16:40:28 +0530

    Modified: implemented try again for invalid condition

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,14 +4,18 @@ import java.util.regex.*;
 public class UserRegistration {
 
     //uc1-first-name
-    static String firstName(){
+    static String firstNameCheck(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter first name:");
+        System.out.println("Enter first name:(start with cap and minimum 3 characters)");
         String fName = scan.nextLine();
         return fName;
     }
     public static void main(String[] args) {
-        boolean res = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*", UserRegistration.firstName());
+        boolean res = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*", UserRegistration.firstNameCheck());
         System.out.println(res);
+        while(res == false){
+            System.out.println("Try again!");
+            res = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*", UserRegistration.firstNameCheck());
+        }
     }
 }
