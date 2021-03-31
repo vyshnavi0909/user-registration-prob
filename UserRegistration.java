10f69054ed0d77e81c286b9af849506e2b37c4e7
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-31 17:36:55 +0530

    ADD: email checker

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,13 +3,14 @@ import java.util.regex.*;
 
 public class UserRegistration {
 
-	 static String fName;
-	 static String lName;
+	static String fName;
+	static String lName;
+    static String email;
 
     //uc1-first-name
     static String firstNameCheck(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter first name:(start with cap and minimum 3 characters)");
+        System.out.println("\nEnter first name:(start with cap and minimum 3 characters)\n");
         fName = scan.nextLine();
         return fName;
     }
@@ -17,36 +18,55 @@ public class UserRegistration {
     //uc2-last-name
     static String lastNameCheck(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter last name:\n(start with cap and minimum 3 characters)");
+        System.out.println("\nEnter last name:\n(start with cap and minimum 3 characters)\n");
         lName = scan.nextLine();
         return lName;
     }
 
-	 //main method
+    //uc3-email
+    static String emailCheck(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nEnter email id:\n(E.g. abc.xyz@bl.co.in\n(abc, bl & co are manditory) and  \n(xyz & in are optional) with precise @ and . positions)\n");
+        email = scan.nextLine();
+        return email;
+    }
+
+	//main method
     public static void main(String[] args) {
 
-		  //checking first name
+		//checking first name
         boolean res1 = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*", UserRegistration.firstNameCheck());
         System.out.println(res1);
 
-		  //if res1-false then try again
+		// if false then try again
         while(res1 == false){
             System.out.println("Try again!");
             res1 = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*", UserRegistration.firstNameCheck());
         }
 
-		  //chekcing last name
+		//chekcing last name
         boolean res2 = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*", UserRegistration.lastNameCheck());
         System.out.println(res2);
 
-		  //if res2-false then try again
+		//if false then try again
         while(res2 == false){
-            System.out.println("Try again!");
+            System.out.println("\nTry again!");
             res2 = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*", UserRegistration.lastNameCheck());
         }
 
-		  //print statements
-		  System.out.println("First Name: " + fName);
-        System.out.println("Last Name: " + lName);
+        //email check
+        boolean res3 = Pattern.matches("^[a][b][c][.][a-z]{3}[@][b][l][.][c][o][.][a-z]{2}", UserRegistration.emailCheck());
+        System.out.println(res3);
+
+        //if false trying again
+        while(res3 == false){
+            System.out.println("\nTry again!");
+            res3 = Pattern.matches("^[a][b][c][.][a-z]{3}[@][b][l][.][c][o][.][a-z]{2}", UserRegistration.emailCheck());
+        }
+
+		//print statements
+		System.out.println("\nFirst Name: " + fName);
+        System.out.println("\nLast Name: " + lName);
+        System.out.println("\nEmail: " + email);
     }
 }
